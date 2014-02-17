a2736830fb7b63b1431994ca4ace43ffa177c803
sennosays <nbs5044@psu.edu>
2014-02-17 16:53:08 -0500

    finished
    This is the end of hw4 for me. I played around with devectorize but
    couldn't get it to work in a practical way.

--- loopsvsmap.jl
+++ loopsvsmap.jl
@@ -26,12 +26,48 @@ function for_integrate(fn, a::Float64, b::Float64, N = 100)
     return my_sum; 
 end
 
-function map_and_reduce(fn, a, b, N = 100)
+function map_and_reduce_integrate(fn, a, b, N = 100)
     dx = (b-a)/N; 
-    x = [a+i*dx for i in 0:N];
+    x = [a+i*dx for i in 1:N-1];
     mapped = map(f,x); 
     my_sum = reduce(+,mapped); 
-    my_sum = my_sum - 0.5*mapped[1] - 0.5*mapped[N+1]; 
+    my_sum += 0.5*(f(a)+f(b)); 
+    my_sum *= dx
+    return my_sum
+end 
+
+function mapreduce_integrate(fn, a, b, N = 100)
+    dx = (b-a)/N; 
+    x = [a+i*dx for i in 1:N-1];
+    my_sum = mapreduce(f,+,x); 
+    my_sum += 0.5*(f(a)+f(b)); 
     my_sum *= dx
     return my_sum; 
 end 
+
+#test the integrators are accurate 
+alpha = 1.0; 
+for_int = for_integrate(f,-alpha,alpha); 
+mar_int = map_and_reduce_integrate(f,-alpha,alpha); 
+mr_int = mapreduce_integrate(f,-alpha,alpha); 
+
+real_int = erf(1.0/sqrt(2)); 
+
+@assert((for_int-real_int)<1e-5)
+@assert((mar_int-real_int)<1e-5)
+@assert((mr_int-real_int)<1e-5)
+
+for_time_4 = @elapsed(for_integrate(f,0.,1.,10000)); 
+for_time_8 = @elapsed(for_integrate(f,0.,1.,100000000)); 
+
+mar_time_4 = @elapsed(map_and_reduce_integrate(f,0.,1.,10000)); 
+mar_time_8 = @elapsed(map_and_reduce_integrate(f,0.,1.,100000000)); 
+
+mr_time_4 = @elapsed(mapreduce_integrate(f,0.,1.,10000)); 
+mr_time_8 = @elapsed(mapreduce_integrate(f,0.,1.,100000000)); 
+
+println("The time from the for loops for 10^4 is: ",for_time_4," and for 10^8: ",for_time_8);
+println("The time for the map and then reduce function for 10^4 is: ",mar_time_4," and for 10^8: ", mar_time_8);  
+println("The time for mapreduce for 10^4 is ",mr_time_4," and for 10^8 is ",mr_time_8); 
+
+
